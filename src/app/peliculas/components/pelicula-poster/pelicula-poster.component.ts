4816cc0d91f9c91d07f7e1485a8edc01c226d243
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-15 23:07:12 -0600

    animation added

--- src/app/peliculas/components/pelicula-poster/pelicula-poster.component.ts
+++ src/app/peliculas/components/pelicula-poster/pelicula-poster.component.ts
@@ -1,5 +1,12 @@
-import { Input } from "@angular/core";
+import { Input, ViewChild } from "@angular/core";
 import { Component, OnInit } from "@angular/core";
+import {
+  EventData,
+  StackLayout,
+  Image,
+  GestureEventData,
+} from "@nativescript/core";
+import { Color } from "@nativescript/core";
 
 @Component({
   selector: "ns-pelicula-poster",
@@ -9,6 +16,24 @@ import { Component, OnInit } from "@angular/core";
 export class PeliculaPosterComponent implements OnInit {
   @Input("img") imgimput: string = "";
   constructor() {}
+  onTap(args: GestureEventData) {
+    let page = <StackLayout>args.object;
+
+    let view: Image = <Image>page.getViewById("lblNS");
+
+    // Get reference to object we want to animate with code
+    view
+      .animate({
+        duration: 60,
+        opacity: 0.5,
+      })
+      .then(() => {
+        // Reset animation
+        setTimeout(() => {
+          view.opacity = 1;
+        }, 50);
+      });
+  }
 
   ngOnInit(): void {}
 }
