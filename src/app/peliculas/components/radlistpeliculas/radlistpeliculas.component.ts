9233f8e3cd38f78e3d6dbff1869650f48af509ae
jorge Luis Pena Real <falmemor@gmail.com>
2022-01-16 21:26:15 -0600

    redirect works

--- src/app/peliculas/components/radlistpeliculas/radlistpeliculas.component.ts
+++ src/app/peliculas/components/radlistpeliculas/radlistpeliculas.component.ts
@@ -1,5 +1,6 @@
 import { Result } from "./../../shared/interfaces/movieInterface";
 import { Component, Input, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "ns-radlistpeliculas",
@@ -10,7 +11,9 @@ export class RadlistpeliculasComponent implements OnInit {
   @Input("movies") moviesInput: Result[];
   @Input("hg") height: number = 250;
   @Input("tl") title: string;
-  constructor() {}
-
+  constructor(private route: Router) {}
   ngOnInit(): void {}
+  onTap(id: string) {
+    this.route.navigate(["/home/", id]);
+  }
 }
--- src/app/peliculas/peliculas-routing.module.ts
+++ src/app/peliculas/peliculas-routing.module.ts
@@ -1,3 +1,4 @@
+import { PeliculasDetailsComponent } from "./components/peliculas-details/peliculas-details.component";
 import { PeliculasComponent } from "./components/peliculas/peliculas.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
@@ -7,6 +8,10 @@ const routes: Routes = [
   {
     path: "",
     component: PeliculasComponent,
+  },
+  {
+    path: ":id",
+    component: PeliculasDetailsComponent,
   },
 ];
 
